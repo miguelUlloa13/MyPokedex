04ccf5228c8218642d0719fb8bf59dfa5f5e21e7
Miguel Ulloa <miguel.bmangelulloa13@gmail.com>
2023-08-11 12:44:40 -0600

    Initial Commit

---
+++ MyPokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPokedex
+//
+//  Created by Miguel Angel Bric Ulloa on 11/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
